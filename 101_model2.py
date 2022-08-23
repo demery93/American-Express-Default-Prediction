a8b000c840f4b698736f951b18872f5bef410dc5
demery93 <72764312+demery93@users.noreply.github.com>
2022-08-22 21:12:51 -0400

    0.800 Update

--- 101_model2.py
+++ 101_model2.py
@@ -1,9 +1,3 @@
-'''
-Model 3 - LightGBM with Focal Loss
-Features Used - 1,500
-CV Score: 0.7976
-# Rounds: 6000
-'''
 import pandas as pd
 import numpy as np
 import lightgbm as lgb
@@ -20,55 +14,82 @@ from utils import FocalLoss
 
 fl = FocalLoss(alpha=.3, gamma=2)
 
-#nfeats = sys.argv[1]
-nfeats = '1500'
 
 train = pd.read_feather("train_features/train.f")
 test = pd.read_feather("test_features/test.f")
+features = np.load(f"feature_selection/top_2500_features.npy")
+
+params = {'objective': 'binary',
+          'metric': 'custom',
+          'learning_rate': 0.01,
+          'max_depth': 8,
+          'num_leaves': 2 ** 8 - 1,
+          'max_bin': 255,
+          'min_child_weight': 10,
+          'reg_lambda': 60,  # L2 regularization term on weights.
+          'colsample_bytree': 0.5,
+          'subsample': 0.9,
+          'nthread': 8,
+          'bagging_freq': 1,
+          'verbose': -1,
+          'seed': 42}
+
 labels = pd.read_csv("input/train_labels.csv")
 sub = pd.read_csv("input/sample_submission.csv")
-features = np.load(f"feature_selection/top_{nfeats}_features.npy")
+
 
 y = labels['target'].values
 print(f"Training on {len(features)} features")
 train = train[features]
 test = test[features]
 gc.collect()
+NFOLD = config.NFOLDS
 
-preds = np.zeros((len(test), config.NBAG))
+kfold = StratifiedKFold(n_splits=NFOLD, random_state=42, shuffle=True)
+oof = np.zeros(len(train))
+preds = np.zeros(len(test))
 # Iterate through each fold
 scores = []
 print(f"Training on {len(features)} features")
-for i in range(config.NBAG):
-    params = {'objective': 'binary',
-              'metric': 'custom',
-              'learning_rate': 0.01,
-              'max_depth': 8,
-              'num_leaves': 2 ** 8 - 1,
-              'max_bin': 255,
-              'min_child_weight': 10,
-              'reg_lambda': 60,  # L2 regularization term on weights.
-              'colsample_bytree': 0.5,
-              'subsample': 0.9,
-              'nthread': 8,
-              'bagging_freq': 1,
-              'verbose': -1,
-              'seed': 42+i}
-    print(f'Training model {i + 1}')
+for fold, (trn_ind, val_ind) in enumerate(kfold.split(train[features], y)):
+    print(f'Training fold {fold + 1}')
+    x_train, x_val = train[features].iloc[trn_ind], train[features].iloc[val_ind]
+    y_train, y_val = y[trn_ind], y[val_ind]
 
-    train_dataset = lgb.Dataset(train[features], y)
+    train_dataset = lgb.Dataset(x_train, y_train)
+    val_dataset = lgb.Dataset(x_val, y_val)
     model = lgb.train(params=params,
                       train_set=train_dataset,
-                      num_boost_round=6000,
-                      fobj=fl.lgb_obj)
+                      valid_sets=[train_dataset, val_dataset],
+                      num_boost_round=10000,
+                      callbacks=[lgb.log_evaluation(100), lgb.early_stopping(500)],
+                      fobj=fl.lgb_obj,
+                      feval=amex_metric_mod_lgbm)
 
-    with open(f"trained_models/lgb_focal_{i}.pkl", 'wb') as file:
+    with open(f"trained_models/model2_{fold}.pkl", 'wb') as file:
         pickle.dump(model, file)
 
-    preds[:,i] = model.predict(test[features])
+    val_pred = model.predict(x_val)
+    test_pred = model.predict(test[features])
 
+    val_pred = pd.Series(val_pred).rank(pct=True)
+    test_pred = pd.Series(test_pred).rank(pct=True)
 
-pd.DataFrame(preds, columns=[f'model2_pred{i}' for i in range(config.NBAG)]).to_csv("output/model2_submission_expanded.csv", index=False, header=True)
+    oof[val_ind] = val_pred
+    preds += test_pred / NFOLD
 
-sub['prediction'] = np.mean(preds, axis=1)
-sub.to_csv(f"output/model2_submission.csv", index=False, header=True)
+    score = amex_metric_mod(y_val, oof[val_ind])
+    scores.append(score)
+    print(f"Fold {fold + 1} score: {score}")
+
+print(f"Average CV Score {np.mean(scores)}")
+print(f"Full CV Score: {amex_metric_mod(y, oof)}")
+#Average CV Score 0.7984511809090076
+#Full CV Score: 0.7981107736140417
+
+sub['prediction'] = preds
+sub.to_csv(f"output/submission_model2.csv", index=False, header=True)
+
+oof_df = pd.read_csv("input/train_labels.csv")
+oof_df['prediction'] = oof
+oof_df[['customer_ID','prediction']].to_csv(f"output/validation_model2.csv", index=False, header=True)
